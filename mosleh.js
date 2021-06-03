ce55167224bba0079322793fac21e043118dc4f4
mohammadmosleh <>
2021-06-03 11:51:41 +0000

    add java

---
+++ mosleh.js
@@ -0,0 +1,10 @@
+var a=prompt("are you guest or employee")
+if (a=="guest"){
+
+  alert('welcome to our company')
+} else if(a=="employee") {
+alert("sign in with your username and password")
+}else { alert("welcome")
+
+}
+document.write("<p>Mosleh Contracting</p>")
