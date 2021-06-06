11b75e988c3786e1555146caa8eafb6d160c410c
mohammadmosleh <>
2021-06-06 10:54:52 +0000

    lab08

--- mosleh.js
+++ mosleh.js
@@ -1,4 +1,11 @@
 var a=prompt("are you guest or employee")
+while(!(a=="guest"||a=="employee")){
+a=prompt("are you guest or employee")
+
+}
+
+
+
 if (a=="guest"){
 
   alert('welcome to our company')
@@ -8,3 +15,8 @@ alert("sign in with your username and password")
 
 }
 document.write("<p>Mosleh Contracting</p>")
+var b=prompt("how many child do you have?")
+var i=0;
+for(var i=0;i<b;i++){
+  document.write("<img src='https://www.cscbroward.org/sites/default/files/styles/blog_/public/2019-08/All%20Children%20Develop%20at%20their%20Own%20Pace.JPG?h=33334fe2&itok=adHi3i7m'/>")
+}
